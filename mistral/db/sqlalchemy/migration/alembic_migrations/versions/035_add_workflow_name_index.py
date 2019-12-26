ead51ed80e1ba705f9f60ab1f67e5f046db82209
James Kylstra <jkylstra@civisanalytics.com>
2019-12-26 12:22:49 -0500

    reconfigure migrations

---
+++ mistral/db/sqlalchemy/migration/alembic_migrations/versions/035_add_workflow_name_index.py
@@ -0,0 +1,22 @@
+"""Add workflow_name index in action_executions_v2 table
+
+Revision ID: 035
+Revises: 034
+Create Date: 2019-12-26 17:21:14.975474
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '035'
+down_revision = '034'
+
+from alembic import op
+
+
+def upgrade():
+    op.create_index(
+        'action_executions_v2_workflow_name',
+        'action_executions_v2',
+        ['worklow_name'],
+        unique=False
+    )
